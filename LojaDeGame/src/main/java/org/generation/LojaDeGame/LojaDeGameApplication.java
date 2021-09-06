3e98cd1a2e2c46916f849ee38c823ff0071ecf07
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-06 15:24:23 -0300

    Commit main

--- LojaDeGame/src/main/java/org/generation/LojaDeGame/LojaDeGameApplication.java
+++ LojaDeGame/src/main/java/org/generation/LojaDeGame/LojaDeGameApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LojaDeGameApplication {
 
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LojaDeGameApplication.class, args);
 	}
