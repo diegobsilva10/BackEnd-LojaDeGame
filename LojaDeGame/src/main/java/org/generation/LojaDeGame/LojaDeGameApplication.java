fdab8f103694b762f4b29e90f9d4ae42f53c77f5
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-06 15:15:37 -0300

    commit

---
+++ LojaDeGame/src/main/java/org/generation/LojaDeGame/LojaDeGameApplication.java
@@ -0,0 +1,13 @@
+package org.generation.LojaDeGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeGameApplication.class, args);
+	}
+
+}
---
+++ LojaDeGame/src/test/java/org/generation/LojaDeGame/LojaDeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.LojaDeGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
