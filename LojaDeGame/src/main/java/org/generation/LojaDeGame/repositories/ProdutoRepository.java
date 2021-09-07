3b90aa8e652e54225d8fe623a3ec86d2d5fe8075
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-07 16:43:29 -0300

    produto repository implementado

--- LojaDeGame/src/main/java/org/generation/LojaDeGame/repositories/ProdutoRepository.java
+++ LojaDeGame/src/main/java/org/generation/LojaDeGame/repositories/ProdutoRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
     public List<ProdutoModel> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
 }
