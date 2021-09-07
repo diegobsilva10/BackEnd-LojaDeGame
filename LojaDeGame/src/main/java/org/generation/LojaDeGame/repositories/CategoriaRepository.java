4e924e038c85fa669fdc643e9f8cca6840c0b24e
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-07 01:48:35 -0300

    inserindo anotação Repository

--- LojaDeGame/src/main/java/org/generation/LojaDeGame/repositories/CategoriaRepository.java
+++ LojaDeGame/src/main/java/org/generation/LojaDeGame/repositories/CategoriaRepository.java
@@ -2,9 +2,11 @@ package org.generation.LojaDeGame.repositories;
 
 import org.generation.LojaDeGame.model.CategoriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
     public List<CategoriaModel> findAllByNomeCategoriaContainingIgnoreCase(String nomeCategoria);
 
