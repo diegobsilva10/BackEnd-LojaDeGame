f49df37daf3dca75ef57889b05933a11d34c6da4
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-07 01:18:44 -0300

    incluindo anotação NotBlank e Size

---
+++ LojaDeGame/src/main/java/org/generation/LojaDeGame/model/ProdutoModel.java
@@ -0,0 +1,33 @@
+package org.generation.LojaDeGame.model;
+/**
+ * Classe utilizada como entidade no banco de dados para a criação das categorias.
+ *
+ * @author Diego Silva
+ * @since 1.0
+ *
+ */
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class ProdutoModel {
+    private static void final long servialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Nome do Produto")
+    @Size(min = 2, max = 50)
+    private String nomeProduto;
+
+    @NotBlank
+    private BigDecimal valor;
+
+    private Date dataHora;
+}
