4bac28d080fca642bebb798ca453d6437083d44c
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-07 16:39:40 -0300

    GET POST PUT DELETE implementado

--- LojaDeGame/src/main/java/org/generation/LojaDeGame/controller/ProdutoController.java
+++ LojaDeGame/src/main/java/org/generation/LojaDeGame/controller/ProdutoController.java
@@ -1,4 +1,67 @@
 package org.generation.LojaDeGame.controller;
 
+
+
+import org.generation.LojaDeGame.model.ProdutoModel;
+import org.generation.LojaDeGame.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    @Autowired
+     private ProdutoRepository repository;
+
+
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoModel>> getAllProdutos() {
+        List<ProdutoModel> produtosList = repository.findAll();
+        if (produtosList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<List<ProdutoModel>>(produtosList, HttpStatus.OK);
+        }
+    }
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findById(@PathVariable long id){
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoModel> post (@RequestBody ProdutoModel produtoModel) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produtoModel));
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity update(@PathVariable("id") long id,
+                                 @RequestBody ProdutoModel produtoModel) {
+        return repository.findById(id)
+                .map(record -> {
+                    record.setNomeProduto(produtoModel.getNomeProduto());
+                    record.setDescricaoProduto(produtoModel.getDescricaoProduto());
+                    record.setQtd(produtoModel.getQtd());
+                    record.setValor(produtoModel.getValor());
+                    ProdutoModel updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity <?> delete(@PathVariable long id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
